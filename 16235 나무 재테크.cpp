daf52cfa6204767efe2ef3ffecfbb1fe238a597d
ans4572 <ans4572@naver.com>
2022-05-03 21:16:37 +0900

    Add files via upload

--- 16235 나무 재테크.cpp
+++ 16235 나무 재테크.cpp
@@ -1,144 +1,126 @@
+// https://www.acmicpc.net/problem/16235
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
-#include<cmath>
-#include<cstring>
 #include<queue>
 
 using namespace std;
 
-#define MAX 11
-using namespace std;
-
 int N, M, K;
-int Energy[MAX][MAX];
-int Insert_Energy[MAX][MAX];
+int nourishment[11][11];   // 각 칸의 추가될 양분
+int map[11][11];           // 현재 양분
+vector<int> trees[11][11];
 
-vector<int> MAP[MAX][MAX];
+int dr[8] = { -1, -1, -1, 0, 0, +1, +1, +1 };
+int dc[8] = { -1, 0, +1, -1, +1, -1, 0, +1 };
 
-int dx[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
-int dy[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+void springAndSummer() {
 
-void SpringAndSummer()
-{
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            if (MAP[i][j].size() == 0) continue;
+	vector<int> deadTrees[11][11]; // 죽는 나무들 저장
 
-            int Die_Tree_Energy = 0;
-            vector<int> Temp;
+	// spring
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			if (trees[i][j].empty())
+				continue;
 
-            sort(MAP[i][j].begin(), MAP[i][j].end());
-            for (int k = 0; k < MAP[i][j].size(); k++)
-            {
-                int Age = MAP[i][j][k];
+			sort(trees[i][j].begin(), trees[i][j].end());
 
-                if (Energy[i][j] >= Age)
-                {
-                    Energy[i][j] = Energy[i][j] - Age;
-                    Temp.push_back(Age + 1);
-                }
-                else
-                {
-                    Die_Tree_Energy = Die_Tree_Energy + (Age / 2);
-                }
-            }
+			for (int k = 0; k < trees[i][j].size(); ++k) {
+				// 양분이 충분한 경우
+				if (map[i][j] >= trees[i][j][k]) {
+					map[i][j] -= trees[i][j][k];
+					trees[i][j][k]++;
+				}
+				// 부족한 경우
+				else {
+					deadTrees[i][j].push_back(trees[i][j][k]);
+					trees[i][j].erase(trees[i][j].begin() + k);
+					k--;
+				}
+			}
+		}
+	}
 
-            MAP[i][j].clear();
-            for (int k = 0; k < Temp.size(); k++)
-            {
-                MAP[i][j].push_back(Temp[k]);
-            }
-            Energy[i][j] = Energy[i][j] + Die_Tree_Energy;
-        }
-    }
+	// summer
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			for (int k = 0; k < deadTrees[i][j].size(); ++k) {
+				map[i][j] += ( deadTrees[i][j][k] / 2 );
+			}
+		}
+	}
+
+	return;
 }
 
-void Fall()
-{
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            if (MAP[i][j].size() == 0) continue;
+void fall() {
 
-            for (int k = 0; k < MAP[i][j].size(); k++)
-            {
-                int Age = MAP[i][j][k];
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			for (int k = 0; k < trees[i][j].size(); ++k) {
+				if (trees[i][j][k] % 5 == 0) {
+					int nr, nc;
+					for (int d = 0; d < 8; ++d) {
+						nr = i + dr[d];
+						nc = j + dc[d];
+						if (nr >= 1 && nr <= N && nc >= 1 && nc <= N) {
+							trees[nr][nc].push_back(1);
+						}
+					}
+				}
+			}
+		}
+	}
 
-                if (Age % 5 == 0)
-                {
-                    for (int a = 0; a < 8; a++)
-                    {
-                        int nx = i + dx[a];
-                        int ny = j + dy[a];
-
-                        if (nx >= 1 && ny >= 1 && nx <= N && ny <= N)
-                        {
-                            MAP[nx][ny].push_back(1);
-                        }
-                    }
-                }
-            }
-        }
-    }
+	return;
 }
 
-void Winter()
-{
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            Energy[i][j] = Energy[i][j] + Insert_Energy[i][j];
-        }
-    }
+void winter() {
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			map[i][j] += nourishment[i][j];
+		}
+	}
+
+	return;
 }
 
-void Solution()
-{
-    for (int i = 0; i < K; i++)
-    {
-        SpringAndSummer();
-        Fall();
-        Winter();
-    }
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    int Answer = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            Answer = Answer + MAP[i][j].size();
-        }
-    }
+	cin >> N >> M >> K;
 
-    cout << Answer << endl;
-}
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			cin >> nourishment[i][j];
+			map[i][j] = 5;
+		}
+	}
 
+	for (int i = 0; i < M; ++i) {
+		int r, c, y;
+		cin >> r >> c >> y;
+		trees[r][c].push_back(y);
+	}
 
-int main(void)
-{
-    cin >> N >> M >> K;
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            cin >> Insert_Energy[i][j];
-            Energy[i][j] = 5;
-        }
-    }
+	for (int i = 0; i < K; ++i) {
+		springAndSummer();
+		fall();
+		winter();
+	}
 
-    for (int i = 0; i < M; i++)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        MAP[a][b].push_back(c);
-    }
+	int ans = 0;
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			ans += trees[i][j].size();
+		}
+	}
 
-    Solution();
+	cout << ans << endl;
 
-    return 0;
+	return 0;
 }
